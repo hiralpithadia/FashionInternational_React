e6e31e937d5730d310813218fde686c8940800cd
Hiral Pithadia <hiral16594@gmail.com>
2022-02-12 14:00:19 +0100

    Updated js

--- src/App.js
+++ src/App.js
@@ -1,16 +1,15 @@
-import { Component } from "react";
-import { Route } from "react-router";
-import Home from "./components/Home";
+import React from 'react'
+import { Route, Routes } from 'react-router-dom'
+import Home from './components/Home';
 
-class App extends Component {
-
-  render() {
+const App = () => {
+    console.log("This is the process.env", process.env.PUBLIC_URL)
   return (
-    <div>
-      <Route exact path='/' component = {Home} />
-    </div>
+    <Routes>
+    <Route exact path="/" element= {<Home></Home>}>
+    </Route>
+    </Routes>
   );
-}
 }
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -1,13 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
 import "bootstrap/dist/css/bootstrap.min.css";
-import { HashRouter } from "react-router-dom";
+import App from './App';
+import { BrowserRouter, HashRouter } from 'react-router-dom';
 
 ReactDOM.render(
-  <HashRouter basename={process.env.PUBLIC_URL}>
-    <App />
-  </HashRouter>,
+
+    <BrowserRouter basename={process.env.PUBLIC_URL}>
+      <App/>
+    </BrowserRouter>,
   document.getElementById('root')
 );
