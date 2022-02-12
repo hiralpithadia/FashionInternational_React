cee606a48b5a383d9f9e430b1984948388fd40e3
Hiral Pithadia <hiral16594@gmail.com>
2022-02-12 14:09:04 +0100

    CLeanup

--- src/App.js
+++ src/App.js
@@ -1,15 +1,13 @@
-import React from 'react'
-import { Route, Routes } from 'react-router-dom'
-import Home from './components/Home';
+import React from "react";
+import { Route, Routes } from "react-router-dom";
+import Home from "./components/Home";
 
 const App = () => {
-    console.log("This is the process.env", process.env.PUBLIC_URL)
   return (
     <Routes>
-    <Route exact path="/" element= {<Home></Home>}>
-    </Route>
+      <Route exact path="/" element={<Home></Home>}></Route>
     </Routes>
   );
-}
+};
 
 export default App;
