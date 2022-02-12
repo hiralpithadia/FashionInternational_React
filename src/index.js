bfdc34e7da93d50b5b102de3380e1c10c5cafff8
Hiral Pithadia <hiral16594@gmail.com>
2022-02-12 11:49:08 +0100

    Added hash router

--- src/index.js
+++ src/index.js
@@ -4,11 +4,11 @@ import './index.css';
 import App from './components/App';
 import "bootstrap/dist/css/bootstrap.min.css";
 import reportWebVitals from './reportWebVitals';
-import { BrowserRouter } from "react-router-dom";
+import { HashRouter } from "react-router-dom";
 
 ReactDOM.render(
-  <BrowserRouter basename="/FashionInternational_React">
+  <HashRouter basename="/FashionInternational_React">
     <App />
-  </BrowserRouter>,
+  </HashRouter>,
   document.getElementById('root')
 );
