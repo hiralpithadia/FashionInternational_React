89b95fe4258a169a0231089c7f478e9ef146b7c4
Hiral Pithadia <hiral16594@gmail.com>
2022-02-12 12:00:44 +0100

    Updated index

--- src/index.js
+++ src/index.js
@@ -1,9 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './components/App';
+import App from './App';
 import "bootstrap/dist/css/bootstrap.min.css";
-import reportWebVitals from './reportWebVitals';
 import { HashRouter } from "react-router-dom";
 
 ReactDOM.render(
